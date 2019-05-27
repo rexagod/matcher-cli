eade6314472c99dfa72549c8c565f75e7516da1c
rexagod <rexagod@gmail.com>
2019-05-28 03:15:49 +0530

    incorporate orbify setup

--- matcher.js
+++ matcher.js
@@ -1,19 +1,35 @@
 // eslint-disable-next-line new-cap
 const vorpal = new require('vorpal')();
 const _commands = require('./src/matcher-commands.js');
+const _orbify = require('./src/matcher-orbify.js');
+const envVars = require('./.env_vars.json');
 
 vorpal
     .delimiter('matcher-cli > ')
     .show();
 
-if (require('./.env_vars.json').ENV === 'PROD') {
-  _commands = require('../node_modules/@matcher-cli/main/src/matcher-commands');
+if (envVars.ENV === 'PROD') {
+  _commands = envVars.COMMAND_ENDPOINT;
+  _orbify = envVars.ORBIFY_ENDPOINT;
 }
 
 // eslint-disable-next-line no-unused-vars
 const {commands} = _commands;
+// eslint-disable-next-line no-unused-vars
+const {orbify} = _orbify;
 
-// add an ENV $PATH ?
+// Pattern-matching
+vorpal
+    .command('match <X> <Y>', 'Outputs pairs of key-points.')
+    .action(orbifyHelper);
+
+function orbifyHelper(args) {
+  const {X, Y} = args;
+  const resolve = (A) => require('path').resolve(A);
+  orbify({X: resolve(X), Y: resolve(Y)});
+}
+
+// Updates, deploys, and other queries
 vorpal
     .command('matcher <query>', `start: Updates matcher.js & locally deploys it.
          clear: Clears console, and exits.`)
---
+++ src/matcher-orbify.js
@@ -0,0 +1,11 @@
+/*
+* `orbify` will work for TWO images at a time
+* which maybe then compared with each other
+* to any number of images the user may supply
+*/
+
+function orbify(images) {
+  console.log(images);
+}
+
+exports.orbify = orbify;
